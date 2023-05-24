cfef2877ddb9819c0ac409b4e73e2035ff31a0e2
lucas-dev <lucasyudi7@gmail.com>
2023-05-24 19:58:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, url_for, redirect 
+
+app = Flask(__name__)
+
+lista = []
+
+@app.route("/")
+def produtos():
+    return render_template("tela_produtos.html", itens = lista)
+
+@app.route("/produto/novo")
+def get_produto_novo():
+    id = int(request.args.get("id"))
+    item = {}
+    if id > 0:
+        item = lista[id-1]
+    return render_template("produto_novo.html",item = item)
+
+@app.route("/produto/novo", methods=["POST"])
+def produto_novo():
+    form_data = request.form
+    index = int(request.args.get("id"))
+    if index > 0:
+        atualizado = {
+            "id": index,
+            "nome": form_data["nome"],
+            "preco": form_data["preco"]
+        }
+        lista[index-1] = atualizado
+    else:
+        novo = {
+            "id": len(lista) + 1,
+            "nome": form_data["nome"],
+            "preco": form_data["preco"]
+        }
+        lista.append(novo)
+
+    return redirect(url_for("produtos"))
+
+@app.route("/produto/excluir")
+def produto_excluir():
+    id = int(request.args.get("id"))
+    lista.remove(lista[id-1])
+    return redirect(url_for("produtos"))
+    
+
+if __name__ == "__main__":
+    app.run("localhost", 5000, True)
